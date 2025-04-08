2924348a3a77dba3912eaa8e06ba2355be5620ee
Francisco Arlem <113395833+ArlemSousa@users.noreply.github.com>
2025-04-07 21:05:38 -0300

    Initial Create Models for Add Migrations

---
+++ FGC.API/Models/Language.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FGC.API.Models
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LanguageName { get; set; }
+    }
+}
---
+++ FGC.API/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿namespace FGC.API.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    namespace FGC.API.Models
+    {
+        public class Users
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(150)]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [MinLength(8)]
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "A senha deve ter no mínimo 8 caracteres, incluindo números, letras maiúsculas e minúsculas e caracteres especiais.")]
+            public string Password { get; set; }
+
+            public ICollection<Games> Games { get; set; } = new List<Games>();
+        }
+    }
+
+}
