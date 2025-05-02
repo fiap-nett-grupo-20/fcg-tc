8d31eb70543350a09fd6b40989a5290501a95cf6
betomperez <betomperez@gmail.com>
2025-05-01 21:50:16 -0300

    Criada interfaces IUserRepository e IGameRepository.

---
+++ src/FCG.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using FCG.Domain.Entities;
+
+namespace FCG.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User> GetByIdAsync(string id);
+    Task<User> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(string id);
+    Task<bool> ExistsByEmailAsync(string email);
+}
