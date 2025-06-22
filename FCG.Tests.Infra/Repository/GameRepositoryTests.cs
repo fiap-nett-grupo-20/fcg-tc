13370ba784e2aba7a75eaffbdd0689aa938f1bc7
betomperez <betomperez@gmail.com>
2025-06-22 15:27:50 -0300

    Refatorado testes de GameRepository para utilizar SQL Server em memória e adicionado testes de unicidade para propriedade Title.

--- FCG.Tests.Infra/Repository/GameRepositoryTests.cs
+++ FCG.Tests.Infra/Repository/GameRepositoryTests.cs
@@ -3,10 +3,11 @@ using FCG.Domain.Entities;
 using FCG.Infra.Data.Context;
 using FCG.Infra.Data.Repository;
 using FCG.Domain.ValueObjects;
+using FCG.Domain.Exceptions;
 
 namespace FCG.Infra.Tests.Repositories;
 
-public class GameRepositoryTests
+public class GameRepositoryTests : IDisposable
 {
     private readonly FCGDbContext _context;
     private readonly GameRepository _repository;
@@ -14,9 +15,11 @@ public class GameRepositoryTests
     public GameRepositoryTests()
     {
         var options = new DbContextOptionsBuilder<FCGDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=FCGTestDB;Trusted_Connection=True;")
             .Options;
         _context = new FCGDbContext(options);
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
         _repository = new GameRepository(_context);
     }
 
@@ -33,5 +36,40 @@ public class GameRepositoryTests
         var savedGame = await _context.Games.FirstOrDefaultAsync();
         Assert.NotNull(savedGame);
         Assert.Equal("Game Title", savedGame.Title);
+    }
+
+    [Fact]
+    public async Task AddAsync_SameGameTitle_ThrowException()
+    {
+        // Arrange
+        var game = new Game("Game Title", 199.99M, "descricao", "terror");
+        await _repository.AddAsync(game);
+
+        var sameGameTitle = new Game("Game Title", 99.99M, "descrição", "casual");
+
+        // Act & Assert
+        await Assert.ThrowsAsync<DbUpdateException>(async () => await _repository.AddAsync(sameGameTitle));
+    }
+
+    [Fact]
+    public async Task UpdateAsync_UpdateTitleThatAlreadyExists_ThrowException()
+    {
+        // Arrange
+        var existingGame = new Game("Game Title", 199.99M, "descricao", "terror");
+        await _repository.AddAsync(existingGame);
+
+        var game = new Game("New Game Title", 199.99M, "descricao", "terror");
+        await _repository.AddAsync(game);
+
+        game.Title = "Game Title";
+
+        // Act & Assert
+        await Assert.ThrowsAsync<DbUpdateException>(async () => await _repository.UpdateAsync(game));
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
     }
 }
