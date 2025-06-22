fdeb20d2da59e79d9afaee145205e34de54982da
betomperez <betomperez@gmail.com>
2025-06-22 15:26:53 -0300

    Criada novas migrations para fletir mudaça de unicidade para Game.Title.

---
+++ FCG.Infra.Data/Migrations/20250621233756_AjusteTituloUnico.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteTituloUnico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "games",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CI_AI",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_Title",
+                table: "games",
+                column: "Title",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_games_Title",
+                table: "games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "games",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AI");
+        }
+    }
+}
